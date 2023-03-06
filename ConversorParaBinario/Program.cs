b8353ea224e304fe72216f76e201b602195a37da
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-06 15:50:14 -0300

    Add project files.

---
+++ ConversorParaBinario/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 0;
+        int quociente = 1;
+        int[] binario = new int[8];
+        int cont = 0;
+
+        Console.WriteLine("Informe o número a ser convertido para binário");
+        numero = int.Parse(Console.ReadLine());
+
+        while (quociente >= 1)
+        {
+            quociente = numero / 2;
+
+            if (numero % 2 == 0)
+            {
+                binario[cont] = 0;
+                cont++;
+            }
+            else
+            {
+                binario[cont] = 1;
+                cont++;
+            }
+            numero = quociente;
+        }
+
+
+
+        Console.WriteLine("Binario: ");
+        for (int i = 0; i <= cont; i++)
+        {
+            Console.WriteLine(binario[i]);;
+        }
+    }
+}
